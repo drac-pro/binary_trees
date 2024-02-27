df2af6576bca6a31649f515628cc837465722969
Darius Tohtin <dariustohtin@gmail.com>
2024-02-27 06:01:17 -0800

    Created 0-binary_tree_node.c

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Creates a binary tree node without a child
+ *
+ * @parent: pointer to the parent node of the node to create
+ * @value: value to put in the new node
+ *
+ * Return: pointer to the new node (SUCCESS) or NULL (FAILURE)
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = new_node->right = NULL;
+
+	return (new_node);
+}

b02d64d4ebe7d1a5a09201c2baf3505ecc61f777
Darius Tohtin <dariustohtin@gmail.com>
2024-02-28 07:32:09 -0800

    Created 18-binary_tree_uncle.c

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - finds the uncle of a node
+ *
+ * @node: pointer to the node to find the uncle
+ *
+ * Return: pointer to the uncle node, otherwise NULL
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node)
+		return (NULL);
+	if (!node->parent)
+		return (NULL);
+	if (!node->parent->parent)
+		return (NULL);
+	if (!node->parent->parent->left && !node->parent->parent->right)
+		return (NULL);
+	if (node->parent == node->parent->parent->left)
+		return (node->parent->parent->right);
+	else
+		return (node->parent->parent->left);
+}

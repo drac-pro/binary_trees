7b7875f2671e37baf6f49d61e4005a2f933c143f
Darius Tohtin <dariustohtin@gmail.com>
2024-02-28 06:33:27 -0800

    Edited 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -49,8 +49,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	l_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	r_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	l_height = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+	r_height = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 
 	return (l_height >= r_height ? l_height : r_height);
 }
